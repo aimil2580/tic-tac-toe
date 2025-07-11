2b27f7414ee5e59faf51c0721fc99c3332476f20
aimilPC <aimil2580@proton.me>
2025-07-11 19:04:37 +0300

    a

---
+++ script.js
@@ -0,0 +1,29 @@
+const tiles = {
+  t1 : document.querySelector(".t1"),
+  t2 : document.querySelector(".t2"),
+  t3 : document.querySelector(".t3"),
+  t4 : document.querySelector(".t4"),
+  t5 : document.querySelector(".t5"),
+  t6 : document.querySelector(".t6"),
+  t7 : document.querySelector(".t7"),
+  t8 : document.querySelector(".t8"),
+  t9 : document.querySelector(".t9"),
+}
+
+let turnCounter = 1;
+const fisrtPlayerSimbol = "X";
+const secondPlayerSimbol = "O";
+let board =[];
+
+for (let tile in tiles){
+  board.push(tiles[tile])
+  tiles[tile].addEventListener("click" ,()=>{
+    turnCounter % 2===0 ? tiles[tile].textContent = fisrtPlayerSimbol : tiles[tile].textContent = secondPlayerSimbol;
+    turnCounter+=1
+
+  
+  })
+}
+
+console.log(board)
+
